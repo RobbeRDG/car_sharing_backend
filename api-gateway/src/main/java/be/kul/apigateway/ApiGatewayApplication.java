efb9da25d2b8d364c498d495b8a67af93ed9b5bb
RobbeRDG <robbedegroeve@gmail.com>
2021-02-13 13:01:52 +0100

    Added circuit breaker to gateway

--- api-gateway/src/main/java/be/kul/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/be/kul/apigateway/ApiGatewayApplication.java
@@ -1,15 +1,34 @@
 package be.kul.apigateway;
 
+import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
+import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
+import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
+import org.springframework.cloud.client.ConditionalOnDiscoveryHealthIndicatorEnabled;
+import org.springframework.cloud.client.circuitbreaker.Customizer;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+
+import java.time.Duration;
 
 @SpringBootApplication
 @EnableEurekaClient
+@EnableCircuitBreaker
 public class ApiGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+	@Bean
+	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+		return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+				.timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3)).build())
+				.circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+				.build());
 	}
 
 }
