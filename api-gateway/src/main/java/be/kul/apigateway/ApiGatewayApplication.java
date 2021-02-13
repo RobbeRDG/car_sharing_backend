e2b4e5390b12ae6517dddf2dfdd65a43baf41b18
RobbeRDG <robbedegroeve@gmail.com>
2021-02-13 16:56:37 +0100

    Implemented Oauth2

--- api-gateway/src/main/java/be/kul/apigateway/ApiGatewayApplication.java
+++ api-gateway/src/main/java/be/kul/apigateway/ApiGatewayApplication.java
@@ -2,13 +2,15 @@ package be.kul.apigateway;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
-import org.springframework.cloud.client.ConditionalOnDiscoveryHealthIndicatorEnabled;
 import org.springframework.cloud.client.circuitbreaker.Customizer;
-import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
@@ -16,12 +18,23 @@ import java.time.Duration;
 
 @SpringBootApplication
 @EnableEurekaClient
-@EnableCircuitBreaker
 public class ApiGatewayApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ApiGatewayApplication.class, args);
 	}
+
+	@Autowired
+	private TokenRelayGatewayFilterFactory filterFactory;
+
+	@Bean
+	public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route("resource", r -> r.path("/resource")
+						.filters(f -> f.filter(filterFactory.apply()))
+						.uri("http://localhost:9000"))
+				.build();
+	}
+
 
 	@Bean
 	public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
